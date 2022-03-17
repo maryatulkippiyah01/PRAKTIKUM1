d4d3b3843661ddc187c02cb49fb01af2d0413323
maryatulkippiyah <maryatulkippiyah01@gmail.com>
2022-03-17 21:28:57 +0700

    percobaan var

---
+++ src/main/kotlin/7_val vs var.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var varVariabel: Int
+    varVariabel = 56 //pemberian nilai pertama
+    println("Pemberian nilai pertama pada varVariabel = "+varVariabel)
+    varVariabel = 78 //pemberian nilai kedua
+    println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
+
+}
