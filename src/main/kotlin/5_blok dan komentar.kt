0949a2a0ec3352f8e5749206e959af7962efb8d8
maryatulkippiyah <maryatulkippiyah01@gmail.com>
2022-03-17 21:19:30 +0700

    percobaan blok dan komentar

---
+++ src/main/kotlin/5_blok dan komentar.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello, saya baru belajar Kotlin!")
+    //Contoh komentar satu baris
+    /*
+    Contoh komentar multi baris
+     */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
