549711b217debc2253f7c9d5e118e429418925d0
maryatulkippiyah <maryatulkippiyah01@gmail.com>
2022-03-17 21:24:29 +0700

    percobaan variabel

---
+++ src/main/kotlin/6_variabel.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    //contoh variabel
+    var dynamicVar = 2
+    println("Contoh dynamic variabel = " + dynamicVar)
+
+    var staticVar: Char = 'A'
+    println("Contoh static variabel = " + staticVar)
+}
